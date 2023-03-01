b52235a1989f9e08db6c512b18dfb0b0302f6ee7
Ernesto Ojeda Arias <eojeda@gmx.es>
2023-03-01 11:54:41 -0500

    Creating web data model

---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -0,0 +1,26 @@
+package guru.springframework.msscbeerservice.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BeerDto {
+    private UUID id;
+    private Integer version;
+    private String beerName;
+    private BeerStyleEnum beerStyle;
+    private OffsetDateTime createDate;
+    private OffsetDateTime lastModifiedDate;
+    private Long upc;
+    private BigDecimal price;
+    private Integer quantityOnHand;
+}
