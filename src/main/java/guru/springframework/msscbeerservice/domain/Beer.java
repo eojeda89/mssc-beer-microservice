4da19af39f96caccc8336276db4a5736ee70883e
Ernesto Ojeda Arias <eojeda@gmx.es>
2023-03-02 11:26:14 -0500

    Creating beer bootstrap

--- src/main/java/guru/springframework/msscbeerservice/domain/Beer.java
+++ src/main/java/guru/springframework/msscbeerservice/domain/Beer.java
@@ -32,7 +32,7 @@ public class Beer {
     @UpdateTimestamp
     private Timestamp lastModifiedDate;
     @Column(unique = true)
-    private Long upc;
+    private String upc;
     private BigDecimal price;
     private Integer minOnHand;
     private Integer quantityToBrew;
